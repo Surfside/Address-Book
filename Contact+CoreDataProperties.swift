95198a0c01f5e98eefb697bf9e84205bef05cdcb
Wayne Hill <waynedhill@live.com>
2017-07-04 06:39:01 -0400

    added Contact+CoreData.swift

---
+++ Contact+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  Contact+CoreDataProperties.swift
+//  Address Book
+//
+//  Created by Wayne Hill on 7/4/17.
+//  Copyright © 2017 Surfside Software Solution. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Contact {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Contact> {
+        return NSFetchRequest<Contact>(entityName: "Contact");
+    }
+
+    @NSManaged public var timestamp: NSDate?
+    @NSManaged public var lastName: String?
+    @NSManaged public var firstName: String?
+    @NSManaged public var email: String?
+    @NSManaged public var phone: String?
+    @NSManaged public var street: String?
+    @NSManaged public var city: String?
+    @NSManaged public var state: String?
+    @NSManaged public var zip: String?
+
+}
